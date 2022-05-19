51498388e2e3676653c213f842a164572f2e18dd
ariqlubis <ariqpin@outlook.com>
2022-05-19 10:24:42 +0700

    update

---
+++ app/src/main/java/com/eighteam/ojek/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.eighteam.ojek.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.eighteam.ojek.R
+import com.eighteam.ojek.ui.auth.AuthActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, AuthActivity::class.java))
+        }, 2000)
+    }
+}
