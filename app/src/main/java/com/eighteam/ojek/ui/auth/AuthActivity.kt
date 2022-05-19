759819aa0eff22bcc9ee9f5282c68f8cedd7d117
ariqlubis <ariqpin@outlook.com>
2022-05-20 02:13:22 +0700

    feat: Update feature

--- app/src/main/java/com/eighteam/ojek/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/eighteam/ojek/ui/auth/AuthActivity.kt
@@ -1,12 +1,46 @@
 package com.eighteam.ojek.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.eighteam.ojek.R
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
+    companion object {
+        const val EXTRA_PAGE_REQUEST = "extra_page_request"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val pageRequest = intent.getIntExtra(EXTRA_PAGE_REQUEST, 0)
+        if(pageRequest == 1) {
+            toolbarSignUp()
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.fragment_signIn, true)
+                .build()
+
+            Navigation.findNavController(findViewById(R.id.authHostFragment))
+                .navigate(R.id.action_signUp, null, navOptions)
+        }
+    }
+
+    fun toolbarSignUpSuccess() {
+        toolbar.visibility = View.GONE
+    }
+
+    fun toolbarSignUp() {
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = "Sign Up"
+        toolbar.subtitle = "Register"
+        toolbar.navigationIcon= resources.getDrawable(R.drawable.icon_back_arrow, null)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 }
--- app/src/main/java/com/eighteam/ojek/ui/auth/signIn/SignInFragment.kt
+++ app/src/main/java/com/eighteam/ojek/ui/auth/signIn/SignInFragment.kt
@@ -1,11 +1,14 @@
 package com.eighteam.ojek.ui.auth.signIn
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.eighteam.ojek.R
+import com.eighteam.ojek.ui.auth.AuthActivity
 
 class SignInFragment : Fragment() {
 
@@ -16,4 +19,16 @@ class SignInFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnSignUp: Button = view.findViewById(R.id.btnSignUp)
+        btnSignUp.setOnClickListener {
+            val signUp = Intent(activity, AuthActivity::class.java)
+            signUp.putExtra(AuthActivity.EXTRA_PAGE_REQUEST, 1)
+            startActivity(signUp)
+        }
+    }
+
+
 }
